5745c207e3f2a99da73813313edf097772205d66
Kamil Slowikowski <kslowikowski@gmail.com>
2015-04-09 11:48:39 -0400

    add and test option -o

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,33 @@
 # Change Log
 
-# 0.1.2 - 2015-04-07
+# 0.1.2 - 2015-04-09
 
 ## Fixed
 
 -   In `collate_all`, check for `prefix-rnaseq-metrics.tsv` file before
     attempting to merge the files into `prefix-all-metrics.tsv`.
+    
+    -   Warnings are no longer printed when a user tries to collate output
+        files after running picardmetrics without the '-r' option.
+
+-   The tests have been updated: the md5sum of final output files is checked.
+    This is more robust than checking content of log files.
 
 ## Changed
 
 -   Removed regular expression from `is_bam` function, so the script will run
     with Bash version 2 released prior to 2004.
+
+-   Previously, output files were written to a directory called
+    'picardmetrics' created in the BAM file's directory. Currently, the output
+    files are written to a user-specified directory or the current directory
+    by default.
+
+-   The `collate_` functions previously accepted the BAM files output by
+    picardmetrics as an argument. Now, the `collate_` functions accept a
+    directory instead and search for the appropriately named files. This means
+    that the BAM files created by Picard may be deleted after running
+    picardmetrics.
 
 # 0.1.1 - 2015-03-20
 
--- test/test.sh
+++ test/test.sh
@@ -3,20 +3,56 @@
 #
 # Test picardmetrics
 
-# Clean up previous runs.
-rm -rf data/project1/sample?/picardmetrics
-rm -f data/*.tsv
-rm -f test/test-run.log
-rm -f test/test-collate.log
+main() {
+  echo -e "$(DATE)\tSTART\ttest_default"
+  test_default
+  echo -e "$(DATE)\tSTART\ttest_rnaseq"
+  test_rnaseq
+  echo -e "$(DATE)\tDONE"
+}
 
-# Run multiple metrics tools such as Picard on each BAM file.
-for f in data/project1/sample?/sample?.bam
-do
-  ./picardmetrics run -r $f 2>&1 >> test/test-run.log
-done
+test_default() {
+  # Clean up previous runs.
+  rm -rf out
+  rm -f test/test-run.log
+  rm -f test/test-collate.log
 
-# Collate the generated tables into project summary tables.
-./picardmetrics collate \
-  data/project1 \
-  data/project1/sample?/picardmetrics/sample?.sorted.bam \
-  &> test/test-collate.log
+  # Run multiple metrics on each BAM file.
+  for f in data/project1/sample?/sample?.bam
+  do
+    ./picardmetrics run -o out $f 2>&1 >> test/test-run.log
+  done
+
+  # Collate the generated tables into project summary tables.
+  ./picardmetrics collate out/project1 out &> test/test-collate.log
+
+  # Confirm that the final output is correct.
+  md5sum -c \
+    <(echo 5f4a27b122a088730f144c93a2100a74 out/project1-all-metrics.tsv)
+}
+
+test_rnaseq() {
+  # Clean up previous runs.
+  rm -rf out
+  rm -f test/test-run.log
+  rm -f test/test-collate.log
+
+  # Run multiple metrics on each BAM file.
+  for f in data/project1/sample?/sample?.bam
+  do
+    ./picardmetrics run -o out -r $f 2>&1 >> test/test-run.log
+  done
+
+  # Collate the generated tables into project summary tables.
+  ./picardmetrics collate out/project1 out &> test/test-collate.log
+
+  # Confirm that the final output is correct.
+  md5sum -c \
+    <(echo 98887a74cfdec74cb60f11f8f69d281e out/project1-all-metrics.tsv)
+}
+
+DATE() {
+  command date +'%Y-%m-%d %H:%M:%S'
+}
+
+main
